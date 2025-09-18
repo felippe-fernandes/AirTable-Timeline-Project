4c1333c801ef719bf61e555b2e0882d97caa0c85
Felippe Fernandes <felippe1000@hotmail.com>
2025-09-18 10:12:34 -0300

    refactor: correct import case for App component and clean up comments in assignLanes function

--- src/utils/assignLanes.ts
+++ src/utils/assignLanes.ts
@@ -1,19 +1,16 @@
 import { TimelineItem } from '../types/timeline';
 
 export const assignLanes = (items: TimelineItem[]): TimelineItem[] => {
-    // Ordenar por data de início
     const sortedItems = [...items].sort((a, b) => a.start.getTime() - b.start.getTime());
 
     const lanes: TimelineItem[][] = [];
 
     sortedItems.forEach(item => {
-        // Encontrar a primeira lane disponível
         let assignedLane = 0;
 
         for (let i = 0; i < lanes.length; i++) {
             const lastItemInLane = lanes[i][lanes[i].length - 1];
 
-            // Se o item atual começa depois que o último da lane termina
             if (!lastItemInLane || item.start >= lastItemInLane.end) {
                 assignedLane = i;
                 break;
@@ -22,12 +19,10 @@ export const assignLanes = (items: TimelineItem[]): TimelineItem[] => {
             assignedLane = i + 1;
         }
 
-        // Criar nova lane se necessário
         if (!lanes[assignedLane]) {
             lanes[assignedLane] = [];
         }
 
-        // Atribuir lane ao item
         item.lane = assignedLane;
         lanes[assignedLane].push(item);
     });
